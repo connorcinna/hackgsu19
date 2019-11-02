e8995a990fc56b2ea49402953087c3c2c02ea709
connorcinna <connorcinna@gmail.com>
2019-11-02 16:01:55 -0400

    added data sample file

--- serial_test.py
+++ serial_test.py
@@ -3,27 +3,42 @@ import time
 import io 
 import matplotlib.pyplot as plt
 import struct
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5 import QtGui
+from PyQt5.QtGui import QPainter, QBrush, QPen 
+from PyQt5.QtCore import Qt
+
+def gui():
+    my_app = QApplication(sys.argv)
+    w = QWidget()
+    w.setWindowTitle('Music Visualizer')
+    w.show()
+    sys.exit(my_app.exec_())
+
+def paintEvent(self, event):
+    painter = QPainter(self)
+    painter.setPen(QPen(Qt.red, 8, Qt.StraightLine))
+    painter.drawRectangle(40,40,400,400)
 
 ser = serial.Serial(
     baudrate = 115200,
     port = '/dev/ttyACM0' #ports are /dev/ttyXXX on Linux, port are 'COMX' on windows
 )
-'''
-issue with this code is that the port we open is going to be platform dependent.
-WSL has some strange issues regarding ports, so for now we're just going to run this on
-Connor's computer.
-'''
+
 i = 0
 while True: #will continuously read data from serial port
     reading_bytes = ser.readline() #returns byte data type
-    print(reading_bytes)
-    reading_int = int.from_bytes(reading_bytes, byteorder='big')
-    print(str(reading_int))
+    #print('reading_bytes: ' + str(reading_bytes))
+    reading_str = str(reading_bytes)
+    reading_str = reading_str[2:len(reading_str)-5]
     #print('reading_str: ' + reading_str)
-    #reading_float = float(int(reading_str))
+    reading_float = float(reading_str)
     #print('reading_float: ' + str(reading_float))
+    print(str(reading_float))
     #plt.scatter(i, reading_float) #cast reading to a float to plot it
-    #plt.pause(0.05) # to allow the graph to update, dont know if this will throw off timing
+    #plt.pause(0.005) # to allow the graph to update, dont know if this will throw off timing
     i += 1
 #plt.show()
+
 ser.close() 
