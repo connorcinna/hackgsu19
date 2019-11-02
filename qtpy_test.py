ddde15870800757708f81a743fc422f76827b106
Michael <mgoldstein32@gatech.edu>
2019-11-02 19:59:44 -0400

    Threads somewhat functional

--- qtpy_test.py
+++ qtpy_test.py
@@ -9,44 +9,34 @@ from PyQt5 import QtGui
 from PyQt5.QtGui import QPainter, QBrush, QPen 
 from PyQt5.QtCore import Qt
 
-def gui(title):
+line = ''
+
+def gui(lock):
     my_app = QApplication(sys.argv)
     w=QWidget()
-    w.setWindowTitle(title)
+    w.setWindowTitle('Visualization')
     w.show()
     sys.exit(my_app.exec_())
 
-def calculation():
-    # read data from file (board doesn't work with this computer)
-    mic_data = open("output.txt",'r')
+def calculation(lock):
+    print(line)
+    line = float(line)
+    volt = line * .0049
 
-    for line in mic_data:
-        print(line)
-        line = float(line)
+# read data from file (board doesn't work with this computer)
+mic_data = open("output.txt",'r')
+lock = threading.Lock()
+t1 = threading.Thread(target=gui, args=(lock,))
+t2 = threading.Thread(target=calculation, args=(lock,))
 
-    
-    # close data file
-    mic_data.close()
+# start threads
+t1.start()
+t2.start()
 
+# pull in microphone data and convert to float, then convert to frequency data
+for line in mic_data:
+    t2.join()
+    t1.join()
 
-
-class my_thread (threading.Thread):
-    def __init__(self,thread_ID,name,purpose):
-        threading.Thread.__init__(self)
-        self.thread_ID = thread_ID
-        self.name = name
-        self.purpose = purpose
-    
-    def run(self):
-        print('Starting ' + self.name)
-        print('Thread purpose: ' + self.purpose)
-        if(purpose == 'calculation'):
-            calculation()
-        elif(purpose == 'gui'):
-            gui('Visualization')
-        else:
-            print('Invalid thread type')
-    
-    def 
-
-
+ # close data file
+mic_data.close()
